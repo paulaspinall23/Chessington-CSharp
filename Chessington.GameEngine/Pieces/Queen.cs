4fd079cb221f9f380defce4db2cf79ad1b3e1f6d
Aspinall <paspinall2015@gmail.com>
2023-10-24 14:00:17 +0100

    Cherry pick Red 11

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -14,20 +14,50 @@ public class Queen : Piece
     {
         var currentSquare = board.FindPiece(this);
         var result = new List<Square>();
-        for (var i = 0; i < 8; i++)
-            result.Add(Square.At(i, i));
 
-        for (var i = 1; i < 8; i++)
-            result.Add(Square.At(i, 8 - i));
+        //directions
+        var directions = new List<Square>()
+        {
+            Square.At(1, 0), //right
+            Square.At(-1, 0), //left 
+            Square.At(0, -1), //up
+            Square.At(0, 1),  //down
+            Square.At(-1, 1), //top right
+            Square.At(-1, -1), //top left 
+            Square.At(1, 1), //bottom right
+            Square.At(1, -1)  //bottom left
+        };
+        foreach (var direction in directions)
+        {
+            for (var i = 1; i < 8; i++)
+            {
+                //setup square = square.At, i * direction
+                var square = Square.At(currentSquare.Row + i*direction.Row, currentSquare.Col + i*direction.Col);
+            
+                //if outside of board = break
+                if (square.Row < 0 || square.Row > 7 || square.Col < 0 || square.Col > 7)
+                {
+                    break;
+                }
 
-        for (var i = 0; i < 8; i++)
-            result.Add(Square.At(currentSquare.Row, i));
+                //piece on square = get piece
+                var pieceOnSquare = board.GetPiece(square);
 
-        for (var i = 0; i < 8; i++)
-            result.Add(Square.At(i, currentSquare.Col));
-
-        result.RemoveAll(s => s == Square.At(currentSquare.Row, currentSquare.Col));
-
+                // if the piece on square = null, add to result
+                if (pieceOnSquare == null)
+                {
+                    result.Add(square);
+                }
+                else
+                {
+                    if (pieceOnSquare.Player != this.Player)
+                    {
+                        result.Add(square);
+                    }
+                    break;
+                }   
+            }
+        }
         return result;
     }
 }
