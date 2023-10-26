6c54722f62a4469888cab157d8d68de74d30fd6f
Aspinall <paspinall2015@gmail.com>
2023-10-26 08:50:20 +0100

    Cleaned up Pawn code

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -13,7 +13,16 @@ public class Pawn : Piece
 
     public override IEnumerable<Square> GetAvailableMoves(Board board)
     {
-        var directions = new List<Square>()
+        var moves = 1;
+        var startingPosition = 1;
+        var lastRow = 7;
+        if (Player == Player.White)
+        {
+            moves = -1;
+            startingPosition = 6;
+            lastRow = 0; 
+        }
+        var captureDirections = new List<Square>()
         {
             Square.At(1, 1),
             Square.At(1, -1), 
@@ -21,64 +30,32 @@ public class Pawn : Piece
         var currentSquare = board.FindPiece(this);
         var result = new List<Square>();
         
-        if (Player == Player.White && currentSquare.Row > 0)
+        if (currentSquare.Row != lastRow && board.GetPiece(Square.At(currentSquare.Row + moves, currentSquare.Col)) == null)
         {
-            if (board.GetPiece(Square.At(currentSquare.Row - 1, currentSquare.Col)) == null)
+            if (currentSquare.Row == startingPosition && board.GetPiece(Square.At(currentSquare.Row + (moves*2), currentSquare.Col)) == null)
             {
-                if (currentSquare.Row == 6 && board.GetPiece(Square.At(currentSquare.Row - 2, currentSquare.Col)) == null)
-                {
-                    result.Add (Square.At(currentSquare.Row - 1, currentSquare.Col));
-                    result.Add (Square.At(currentSquare.Row - 2, currentSquare.Col));
-                }
-                else
-                {
-                    result.Add (Square.At(currentSquare.Row - 1, currentSquare.Col));
-                }
+                result.Add (Square.At(currentSquare.Row + moves, currentSquare.Col));
+                result.Add (Square.At(currentSquare.Row + (moves*2), currentSquare.Col));
             }
-            foreach (var direction in directions)
+            else
             {
-                var square = Square.At(currentSquare.Row - direction.Row, currentSquare.Col - direction.Col);
-                if (square.Row < 0 || square.Row > 7 || square.Col < 0 || square.Col > 7)
-                {
-                    break;
-                }
-                var pieceOnSquare = board.GetPiece(square);
-                
-                if (pieceOnSquare != null && pieceOnSquare.Player != this.Player)
-                {
-                    result.Add(square);
-                }
-            } 
-        }
-        else if (Player == Player.Black && currentSquare.Row < 7)
-        {
-            if (board.GetPiece(Square.At(currentSquare.Row + 1, currentSquare.Col)) == null)
-            {
-                if (currentSquare.Row == 1 && board.GetPiece(Square.At(currentSquare.Row + 2, currentSquare.Col)) == null)
-                {
-                    result.Add (Square.At(currentSquare.Row + 1, currentSquare.Col));
-                    result.Add (Square.At(currentSquare.Row + 2, currentSquare.Col));
-                }
-                else
-                {
-                    result.Add (Square.At(currentSquare.Row + 1, currentSquare.Col));
-                }
-            }
-            foreach (var direction in directions)
-            {
-                var square = Square.At(currentSquare.Row + direction.Row, currentSquare.Col + direction.Col);
-                if (square.Row < 0 || square.Row > 7 || square.Col < 0 || square.Col > 7)
-                {
-                    break;
-                }
-                var pieceOnSquare = board.GetPiece(square);
-                
-                if (pieceOnSquare != null && pieceOnSquare.Player != this.Player)
-                {
-                    result.Add(square);
-                }
+                result.Add (Square.At(currentSquare.Row + moves, currentSquare.Col));
             }
         }
-        return result;
+        foreach (var captureDirection in captureDirections)
+        {
+            var square = Square.At(currentSquare.Row + (captureDirection.Row * moves), currentSquare.Col + (captureDirection.Col * moves));
+            if (square.Row < 0 || square.Row > 7 || square.Col < 0 || square.Col > 7)
+            {
+                break;
+            }
+            var pieceOnSquare = board.GetPiece(square);
+            
+            if (pieceOnSquare != null && pieceOnSquare.Player != this.Player)
+            {
+                result.Add(square);
+            }
+        }    
+    return result;
     }
 }
